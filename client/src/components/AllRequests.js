8c355bfd2c0c5e262ec4fc904f197839cb25920b
Nancy Do <nancydo272@gmail.com>
2022-09-10 14:08:29 -0400

    all request page

---
+++ client/src/components/AllRequests.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect } from 'react'; 
+import axios from 'axios'; 
+import {Link, useNavigate} from 'react-router-dom'; 
+
+
+const AllRequests = () => {
+    
+    const [requestList, setRequestList] = useState([]); 
+    const navigate = useNavigate(); 
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/callbells')
+        .then((res)=>{
+            setRequestList(res.data)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }, [])
+    
+    const deleteHandler =(id)=>{
+        axios.delete(`http://localhost:8000/api/callbells/${id}`)
+        .then((res)=>{
+            const newRequestList = requestList.filter((request)=>{
+                return request._id !== id
+            })
+            setRequestList(newRequestList)
+            navigate('/')
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+    return (
+        <div> 
+            <div className="d-flex justify-content-between">
+                <h1>Welcome to 5W/NW!</h1>
+                <Link to ={'/login'}>Login</Link>
+            </div>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Room #</th>
+                        <th>Request</th>
+                        <th>Urgency</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+            
+                {
+                    requestList.map((request)=>(
+                    <tr>
+                        <td>{request.room}</td>
+                        <td>{request.request} {request.custom}</td>
+                        <td>{request.urgency}</td>
+                        <td>
+                            <Link to={`/requests/${request._id}`}>Details</Link>  |
+                            <Link to={`/requests/${request._id}/edit`}>Edit</Link> |
+                            <button className="btn btn-danger" onClick={()=>deleteHandler(request._id)} >Done</button>
+                        </td>
+                    </tr>
+                ))
+            }
+            </table>
+        </div>
+    )
+}
+
+export default AllRequests
