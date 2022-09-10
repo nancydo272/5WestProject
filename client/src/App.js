5d2c69a3b424ccf0ef1d00bb8d9bb25ab0013935
Nancy Do <nancydo272@gmail.com>
2022-09-10 10:43:42 -0400

    starting client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import AddRequest from './components/AddRequest'; 
+import EditRequest from './components/EditRequest'; 
+import OneRequest from './components/OneRequest'; 
+import AllRequests from './components/AllRequests'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path ="/unitDashboard" element ={<AllRequests />}/>
+          <Route path ="/requests/:id" element ={<OneRequest />}/>
+          <Route path ="/requests/new" element ={<AddRequest />}/>
+          <Route path ="/requests/:id/edit" element ={<EditRequest />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
